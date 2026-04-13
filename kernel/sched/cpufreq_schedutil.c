b805d2a0baaccc80c71bab77ae175a08bd7e0130
EmanuelCN <emanuelghub@gmail.com>
2026-04-13 23:16:12 +0300

    cpufreq: schedutil: Refine DVFS headroom and suppress low-util boosting
    The current DVFS headroom calculation applies a quadratic taper at the
    high end of the capacity scale but can lead to unnecessary frequency
    spikes at very low utilization levels.
    
    Modify the headroom logic to:
    - Increase the base headroom amount by changing the right-shift from
      12 to 11, providing more aggressive boosting in the active range.
    - Introduce a suppression threshold at 18.75% of capacity
      to reduce power consumption during light workloads..
    
    This ensures that the system remains responsive under moderate load
    while remaining conservative when the CPU is barely active.
    
    Signed-off-by: EmanuelCN <emanuelghub@gmail.com>
    Signed-off-by: skye-tachyon <sonicgames772@gmail.com>
    Signed-off-by: //. <174224977+skye-tachyon@users.noreply.github.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -231,18 +231,18 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 static inline unsigned long apply_dvfs_headroom(unsigned long util, int cpu)
 {
 	unsigned long capacity = capacity_orig_of(cpu);
-	unsigned long delta, headroom, min_util;
+	unsigned long threshold = (capacity >> 3) + (capacity >> 4); /* 18.75% */
+	unsigned long delta, headroom;
 
-	if (util >= capacity)
-		return util;
         /*
-        * Quadratic taper the boosting at the top end as these are expensive
-        * and we don't need that much of a big headroom as we approach max
-        * capacity
-        */
+         * Quadratic taper the boosting at the top end as these are expensive
+         * and we don't need that much of a big headroom as we approach max
+         * capacity
+         */
 	delta = capacity - util;
-	headroom = ((delta * delta) >> 12);
+	headroom = (delta * delta) >> 11;
 
+<<<<<<< HEAD
 	/* 10% of capacity threshold */
 	min_util = capacity / 10;
 
@@ -251,6 +251,13 @@ static inline unsigned long apply_dvfs_headroom(unsigned long util, int cpu)
 		headroom = (headroom * util * util) / (min_util * min_util);
 
 	return util + headroom;
+=======
+	/* Suppress boosting below the threshold */
+	if (util < threshold)
+		headroom = (headroom * util * util) / (threshold * threshold);
+
+	return  util + headroom;
+>>>>>>> 372b6ca522f3 (cpufreq: schedutil: Refine DVFS headroom and suppress low-util boosting)
 }
 
 unsigned long sugov_effective_cpu_perf(int cpu, unsigned long actual,
