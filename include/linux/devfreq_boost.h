2a5cfa89be0d4b28e2481765e0cd75fe1895e76f
Lazy <adriantachyon69@gmail.com>
2026-07-14 21:49:16 +0000

    devfreq_boost.h: DDR boost on touch

--- include/linux/devfreq_boost.h
+++ include/linux/devfreq_boost.h
@@ -4,21 +4,23 @@
  */
 #ifndef _DEVFREQ_BOOST_H_
 #define _DEVFREQ_BOOST_H_
-
 #include <linux/devfreq.h>
-
 enum df_device {
 	DEVFREQ_CPU_LLCC_DDR_BW,
 	DEVFREQ_MAX
 };
-
 #ifdef CONFIG_DEVFREQ_BOOST
 void devfreq_boost_kick(enum df_device device);
+void devfreq_boost_kick_duration(enum df_device device, unsigned int duration_ms);
 void devfreq_boost_kick_max(enum df_device device, unsigned int duration_ms);
 void devfreq_register_boost_device(enum df_device device, struct devfreq *df);
 #else
 static inline
 void devfreq_boost_kick(enum df_device device)
+{
+}
+static inline
+void devfreq_boost_kick_duration(enum df_device device, unsigned int duration_ms)
 {
 }
 static inline
@@ -30,5 +32,4 @@ void devfreq_register_boost_device(enum df_device device, struct devfreq *df)
 {
 }
 #endif
-
 #endif /* _DEVFREQ_BOOST_H_ */
